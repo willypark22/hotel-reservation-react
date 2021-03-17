40a76adf175d355510f5b02bd44b0f0fd2de4fcc
Will Park <willypark22@Wills-MacBook-Pro.local>
2021-03-17 13:50:05 -0700

    Added routing and background images for pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/rooms/">
+          <Rooms />
+        </Route>
+        <Route exact path="/rooms/:slug">
+          <SingleRoom />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleRoom = () => {
+    return (
+        <div>
+            Hello from Single Rooms
+        </div>
+    )
+}
+
+export default SingleRoom;
