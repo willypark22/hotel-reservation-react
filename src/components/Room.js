9ed821e79b074d7bd4f3e8831f07f72d2bd0ba37
Will Park <willypark22@Wills-MacBook-Pro.local>
2021-03-22 10:24:23 -0700

    Finished adding images and text to featured rooms from context api

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import defaultImg from '../images/room-1.jpeg';
 
 const Room = ({ room }) => {
     const {name, slug, images, price} = room;
     return (
         <article className="room">
             <div className="img-container">
-                <img src={images[0]} alt="Single Room" />
+                <img src={images[0] || defaultImg} alt="Single Room" />
+                <div className="price-top">
+                    <h6>${price}</h6>
+                    <p>per night</p>
+                </div>
+                <Link to={`/rooms/${slug}`}  className="btn-primary room-link">
+                    Features
+                </Link>
             </div>
+            <p className="room-info">
+                {name}
+            </p>
         </article>
     )
 }
