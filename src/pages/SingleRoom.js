7de983c1a194b387756dba58751d26773e50dd2a
Will Park <dpark032@ucr.edu>
2021-03-23 12:14:40 -0700

    Populated Rooms page with info

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -51,6 +51,31 @@ const SingleRoom = () => {
                         ))
                     }
                 </div>
+                <div className="single-room-info">
+                    <article className="desc">
+                        <h3>details</h3>
+                        <p>{description}</p>
+                    </article>
+                    <article className="info">
+                        <h3>info</h3>
+                        <h6>price: ${price}</h6>
+                        <h6>size: {size} SQFT</h6>
+                        <h6>Max Capacity: {capacity} {capacity > 1 ? "people" : "person"}</h6>
+                        {/* <h6>Max Capacity: {capacity > 1 ? `${capacity} People` : `${capacity} Person`}</h6> */}
+                        <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+                        <h6>{breakfast && "free breakfast included"}</h6>
+                    </article>
+                </div>
+            </section>
+            <section className="room-extras">
+                <h6>Extras</h6>
+                <ul className="extras">
+                    {
+                        extras.map((item, index) => (
+                            <li key={index}>- {item}</li>
+                        ))
+                    }
+                </ul>
             </section>
         </>
     )
