d9776f7952f510bd2a9cbaddd26f23c5616045fc
Will Park <dpark032@ucr.edu>
2021-03-25 14:07:44 -0700

    Connected Contentful API to app, but did not push contentful.js for api key

--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, Component } from 'react';
-import items from './data';
+// import items from './data';
+import Client from './Contentful';
 
 const RoomContext = React.createContext();
 
@@ -19,23 +20,53 @@ export default class RoomProvider extends Component {
       breakfast: false,
       pets: false
     };
+
+    // Retrieve data from contentful
+    getData = async () => {
+      try {
+        let response = await Client.getEntries({
+          content_type: "beachResortRoom",
+          // order: "sys.createdAt"
+          order: "fields.price"
+        });
+
+        let rooms = this.formatData(response.items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
+  
+        this.setState({
+          rooms,
+          featuredRooms,
+          sortedRooms: rooms,
+          loading: false,
+          price: maxPrice,
+          maxPrice,
+          maxSize
+        });
+      }
+      catch(error) {
+        console.log(error);
+      }
+    }
   
     componentDidMount() {
-      let rooms = this.formatData(items);
-      let featuredRooms = rooms.filter(room => room.featured === true);
-      let maxPrice = Math.max(...rooms.map(item => item.price));
-      let maxSize = Math.max(...rooms.map(item => item.size));
+      this.getData();
+      // let rooms = this.formatData(items);
+      // let featuredRooms = rooms.filter(room => room.featured === true);
+      // let maxPrice = Math.max(...rooms.map(item => item.price));
+      // let maxSize = Math.max(...rooms.map(item => item.size));
 
-      this.setState({
-        rooms,
-        featuredRooms,
-        sortedRooms: rooms,
-        loading: false,
-        price: maxPrice,
-        maxPrice,
-        maxSize
-      });
-      console.log(this.state);
+      // this.setState({
+      //   rooms,
+      //   featuredRooms,
+      //   sortedRooms: rooms,
+      //   loading: false,
+      //   price: maxPrice,
+      //   maxPrice,
+      //   maxSize
+      // });
+      // console.log(this.state);
     }
   
     formatData(items) {
